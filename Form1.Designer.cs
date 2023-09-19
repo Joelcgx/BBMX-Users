7f0a16b55386100f18ecdfdca1a67b11fbc4dfd2
Joel Corrales <godinezjoel727@gmail.com>
2023-09-18 18:56:17 -0700

    First Form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            btn_log_in = new Button();
+            btn_cancel = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(176, 25);
+            label1.TabIndex = 0;
+            label1.Text = "BIenvenido a BBMX";
+            // 
+            // btn_log_in
+            // 
+            btn_log_in.Location = new Point(698, 415);
+            btn_log_in.Name = "btn_log_in";
+            btn_log_in.Size = new Size(90, 23);
+            btn_log_in.TabIndex = 1;
+            btn_log_in.Text = "Iniciar Sesion";
+            btn_log_in.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancel
+            // 
+            btn_cancel.Location = new Point(602, 415);
+            btn_cancel.Name = "btn_cancel";
+            btn_cancel.Size = new Size(90, 23);
+            btn_cancel.TabIndex = 2;
+            btn_cancel.Text = "Cancelar";
+            btn_cancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_cancel);
+            Controls.Add(btn_log_in);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "BBMX-Inciar Sesion";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button btn_log_in;
+        private Button btn_cancel;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,26 @@ namespace BBMX_Users
         public Form1()
         {
             InitializeComponent();
+            //Eventos HAndler
+            btn_log_in.Click += new EventHandler(iniciar_sesion);
+            btn_cancel.Click += new EventHandler(salir);
+        }
+        //Funcion de los widgets
+        private void iniciar_sesion(object sender, EventArgs e)
+        {
+
+        }
+        private void salir(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Realmente desea salir?", "Esta seguro?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                // No haces nada aquí y el cuadro de mensaje se cierra
+            }
         }
     }
 }
